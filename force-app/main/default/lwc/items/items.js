a28de02d7ff8a57fc41242bcc99c66dde2b43215
Timothy <tdionne@salesforce.com>
2022-11-09 12:35:37 -0800

    fix typos

--- force-app/main/default/lwc/items/items.js
+++ force-app/main/default/lwc/items/items.js
@@ -5,10 +5,10 @@ export default class Items extends LightningElement {
 
     updateItems() {
         // This wil not work because you are not setting the tracked property
-        // but instead or simply updating an element in the array.
+        // but instead are simply updating an element in the array.
         // this.items[1] = this.items[1].toUpperCase();
 
-        // This will work because we are cloning the tracked proeprty (array)
+        // This will work because we are cloning the tracked property (array)
         // changing it as desired, and then assigning the result back to the
         // tracked property.
         const _items = [...this.items];
