a7066bf09b87dbcd6b1db7bd0aaf85836391f690
Timothy <tdionne@salesforce.com>
2022-11-09 12:14:44 -0800

    Init

---
+++ force-app/main/default/lwc/items/items.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+
+export default class Items extends LightningElement {
+    items = ['one', 'two', 'three'];
+
+    updateItems() {
+        // This wil not work because you are not setting the tracked property
+        // but instead or simply updating an element in the array.
+        // this.items[1] = this.items[1].toUpperCase();
+
+        // This will work because we are cloning the tracked proeprty (array)
+        // changing it as desired, and then assigning the result back to the
+        // tracked property.
+        const _items = [...this.items];
+        _items[1] = _items[1].toUpperCase();
+        this.items = _items;
+    }
+}
